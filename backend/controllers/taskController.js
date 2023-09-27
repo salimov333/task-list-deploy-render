d3313cf2ac851d07b76dce1ab459536372b780f9
salimov333 <salimov333@gmail.com>
2023-09-27 10:05:42 +0200

    initial commit

---
+++ backend/controllers/taskController.js
@@ -0,0 +1,85 @@
+import fs from "fs/promises";
+
+export const createTask = async (req, res, next) => {
+    try {
+        await fs.appendFile("db.txt", `+${JSON.stringify(req.body)}`)
+        console.log("Task created!");
+        // throw new Error("Our artificial error")
+        res.status(201).json({ msg: "New task created!" })
+    } catch (error) {
+        const errorResponse = {
+            status: 500,
+            error: "Internal server error!"
+        }
+        next(errorResponse);
+        // res.status(500).json({msg:"Internal server error!"});
+    }
+};
+
+export const getAllTasks = async (req, res) => {
+    try {
+        const data = await fs.readFile("db.txt", "utf-8");
+        const dataArray = data.split("+").filter(Boolean);
+        const jsonObjects = dataArray.map((elem) => JSON.parse(elem));
+        res.status(200).json(jsonObjects)
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ msg: "Internal server error!" });
+    }
+};
+
+export const getOneTask = async (req, res) => {
+
+};
+export const updateOneTask = async (req, res) => {
+
+};
+export const deleteOneTask = async (req, res) => {
+
+};
+export const deleteAllTasks = async (req, res) => {
+    try {
+        await fs.writeFile('db.txt', '');
+        res.status(200).json({ msg: "All tasks deleted!" });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ msg: "Internal server error!" });
+    }
+};
+
+// app.get("/getTasks", (req, res)=>{
+//     console.log("inside getTasks");
+//     fs.readFile("db.txt", "utf-8")
+//     .then((data)=>{
+//         const dataArray = data.split("+").filter(Boolean);
+//         const jsonObjects = dataArray.map((elem)=> JSON.parse(elem));
+//         console.log({jsonObjects});
+//         res.status(200).json(jsonObjects);
+//     })
+//     .catch((err)=>{
+//         console.log(err);
+//     })
+// });
+
+// app.post("/createTask", (req, res)=>{
+//     console.log("body", req.body);
+//     fs.appendFile("db.txt", `+${JSON.stringify(req.body)}`)
+//     .then(()=>{
+//         console.log("Task added!");
+//         res.status(200).json({message:"Task created!"})
+//     })
+//     .catch((error)=>{
+//         console.log({error});
+//     })
+// });
+
+// app.delete("/deleteTasks", (req, res)=>{
+//     fs.writeFile("db.txt", "")
+//     .then(()=>{
+//         console.log("Tasks deleted!");
+//         res.status(200).json({message:"All tasks deleted!"})
+//     })
+//     .catch((error)=>{
+//         console.log(error);
+//     })
+// })
---
+++ src/api.js
@@ -0,0 +1,56 @@
+const URL = "/tasks"
+
+export const createTask = async (data) => {
+    try {
+        await fetch(URL, {
+            method: "POST",
+            body: JSON.stringify(data),
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+export const getAllTasks = async () => {
+    try {
+        const response = await fetch(URL);
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+export const updateOneTask = async (id, data) => {
+    try {
+        await fetch(`${URL}/${id}`, {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(data)
+        });
+
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+export const deleteAllTasks = async () => {
+    try {
+        await fetch(URL, { method: "DELETE" })
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+export const deleteOneTask = async (id) => {
+    try {
+        await fetch(`${URL}/${id}`, { method: "DELETE" });
+    } catch (error) {
+        console.log(error);
+    }
+}
