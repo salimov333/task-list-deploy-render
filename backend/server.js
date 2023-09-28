c002429be0df85dd8a32e1c0bbc3bbfabf45d386
salimov333 <salimov333@gmail.com>
2023-09-28 09:48:15 +0200

    backend ready to deploy

--- backend/server.js
+++ backend/server.js
@@ -3,10 +3,7 @@ import dotenv from "dotenv";
 dotenv.config();
 import cors from "cors";
 import taskRouter from "./routes/taskRouter.js";
-import path from "path";
-import { fileURLToPath } from "url";
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+
 
 const app = express();
 
@@ -16,8 +13,6 @@ app.use(cors());
 app.use(express.json()); // body parser erlaubt uns auf Daten aus dem req.body zuzugreifen
 
 app.use("/tasks", taskRouter);
-app.use("/", express.static(path.join(__dirname, "/dist")));
-app.get("/*", (req, res) => res.sendFile(__dirname + "/dist/index.html"));
 
 app.use((req, res, next) => {
     res.status(404).send("Page not found!");
