61a7bf0cf6041e1cf79d34b22369ce17d257912a
salimov333 <salimov333@gmail.com>
2023-09-28 10:49:03 +0200

    deleted cors

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ const app = express();
 
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+// app.use(cors());
 app.use(express.json()); // body parser erlaubt uns auf Daten aus dem req.body zuzugreifen
 
 app.use("/tasks", taskRouter);
